f84bfe272616130560726edbc67ac20a786f7676
Suprateek Sen <suprateeksen62@gmail.com>
2025-03-15 10:40:35 +0530

    First Commit

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -5,7 +5,7 @@ import ReviewCard from '../Components/ReviewCard'
 const CustomerReviews = () => {
   return (
    <section className='max-container'>
-    <h2 className='font-palanquin text-4xl font-bold text-center'>What Our <span className='text-coral-red'>Customers</span>  Say ?</h2>
+    <h2 className='font-palanquin text-4xl font-bold text-center'>Chukuchu ki re<span className='text-coral-red'>Magia</span></h2>
     <p className='info-text m-auto mt-4 max-w-lg text-center text-slate-gray font-montserrat'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea exercitationem quae quos.</p>
     <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
       {reviews.map((review,index)=>(
