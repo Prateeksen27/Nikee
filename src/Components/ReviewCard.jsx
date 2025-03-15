a130289d825488479b0a3b1bb652ab1bc0969a8b
Suprateek Sen <suprateeksen62@gmail.com>
2025-03-16 02:04:25 +0530

    Features added

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { star } from '../assets/icons'
+import  halfStar  from '../assets/icons/halfstar.svg'
 
 const ReviewCard = ({imgURL,customerName,rating,feedback,key}) => {
   return (
-    <div>ReviewCard</div>
+    <div className='flex justify-center items-center flex-col'>
+        <img src={imgURL} alt="" className='rounded-full object-cover w-[120px] h-[120px]' />
+        <p className='text-lg font-bold mt-4'>{customerName}</p>
+        <p className='mt-6 max-w-sm text-center info-text mb-6'>{feedback}</p>
+        <div className='flex justify-center items-center gap-2'>
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+            <img src={star} alt="" />
+            <img width={24} height={24} src={halfStar} alt="" />
+            <p className='font-montserrat text-lg text-slate-gray'>{rating}</p>
+        </div>
+    </div>
   )
 }
 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Button from '../Components/Button'
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className='max-container flex justify-between items-center max-lg:flex-col gap-10 w-full'>
+     
+        <h1 className='font-palanquin lg:max-w-md leading-[68px] text-4xl font-bold'>Sign Up for <span className='text-coral-red'>Updates</span> <br /> & Newsletters</h1>
+        <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col'>
+          <input type="text" placeholder='subscribe' className='input' />
+          <div className='flex max-sm:justify-end items-center max-sm:w-full' >
+            <Button label='Sign Up' />
+          </div>
+        </div>
+     
+     
+    </section>
   )
 }
 
