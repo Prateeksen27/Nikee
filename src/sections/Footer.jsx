5363de150c2388d8c27e7ca75431fc63c1f410c6
Suprateek Sen <suprateeksen62@gmail.com>
2025-03-16 11:23:56 +0530

    Footer Completed

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import footerLogo from '../assets/images/footer-logo.svg'
+import { socialMedia, footerLinks } from '../Constants/Constants'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='max-container flex justify-between items-start gap-20 flex-wrap max-lg:flex-col'>
+      <div className='flex flex-col items-start gap-4'>
+        <img src={footerLogo} alt="" className='w-32 h-32 object-contain' />
+        <div className='flex gap-4'>
+          {socialMedia.map((social, index) => (
+            <div key={index} className='w-12 h-12 flex justify-center items-center cursor-pointer rounded-full bg-white'>
+              <img src={social.src} alt={social.id} className='w-1/2 h-1/2 object-contain' />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap'>
+          {footerLinks.map((section, index) => (
+            <div key={index}>
+              <h4 className='text-white font-montserrat text-2xl leading-normal font-medium mb-6'>{section.title}</h4>
+              <ul>
+                {section.links.map((link, index) => (
+                  <li key={index} className='mt-4 font-montserrat text-base leading-normal text-white hover:text-slate-gray cursor-pointer'>{link.name}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+      </div>
+    </section>
   )
 }
 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import Button from '../Components/Button'
 const Subscribe = () => {
   return (
-    <section className='max-container flex justify-between items-center max-lg:flex-col gap-10 w-full'>
-     
-        <h1 className='font-palanquin lg:max-w-md leading-[68px] text-4xl font-bold'>Sign Up for <span className='text-coral-red'>Updates</span> <br /> & Newsletters</h1>
-        <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col'>
-          <input type="text" placeholder='subscribe' className='input' />
-          <div className='flex max-sm:justify-end items-center max-sm:w-full' >
-            <Button label='Sign Up' />
-          </div>
-        </div>
-     
-     
-    </section>
+  <section className='max-container flex justify-between items-center max-lg:flex-col gap-10'>
+    <h3 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>Sign Up for <span className='text-coral-red'>Updates</span> & <span className='text-coral-red'>Newsletters</span></h3>
+    <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-1 sm:border sm:border-slate-gray rounded-full'>
+      <input type="text" placeholder='Subscribe to our newsletter' className='input' />
+      <div className='flex max-sm:justify-end items-center max-sm:w-full'>
+        <Button label='Sign Up' className='w-full' />
+      </div>
+    </div>
+  </section>
   )
 }
 
